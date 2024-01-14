cb66e62000cd29ca0f428e11ee8f228c54c5044e
NikolaPejchinovski <niklaus420nikola@gmail.com>
2024-01-14 22:10:44 +0100

    change

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ randomQuote();
 
 //This takes the noise name which also is the page name. It allows me to put everything in the same js file
 let noiseToPlayId = location.pathname.substring(1, location.pathname.lastIndexOf('.'));
+console.log(noiseToPlayId);
 const noiseToPlay = document.getElementById(noiseToPlayId);
 
 //Randomize waves on start
